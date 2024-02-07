b166467d0cabeefa652a7df4525236fc60a62bf9
Arlid Henao Rueda <arlidhenaorueda@gmail.com>
2024-02-06 20:37:46 -0500

    Initial Commit

---
+++ eliteapartmentsapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eliteapartmentsapp
+//
+//  Created by Arlid Henao Rueda on 6/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eliteapartmentsapp/eliteapartmentsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  eliteapartmentsappApp.swift
+//  eliteapartmentsapp
+//
+//  Created by Arlid Henao Rueda on 6/02/24.
+//
+
+import SwiftUI
+
+@main
+struct eliteapartmentsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
